605828d6b1a4b5ea4fa4c73f29eb11c04d2c0c1b
Jonathan <jonijanzen@t-online.de>
2024-10-25 15:49:25 +0200

    change3

--- SEI/Program.cs
+++ SEI/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World! Test test");
             Console.WriteLine("BROOOOOOOOOOOOOOO");
+            Console.WriteLine("testsettse");
         }
     }
 }
