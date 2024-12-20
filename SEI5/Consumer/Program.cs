997f848b89a5ec83a82be385a744064796d6effd
Jonathan <jonijanzen@t-online.de>
2024-12-20 17:15:08 +0100

    SEI5

---
+++ SEI5/Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Consumer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "SEI5", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            Console.WriteLine("Consumer started");
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($"Received message: {message}");
+                return Task.CompletedTask;
+            };
+
+            await channel.BasicConsumeAsync("SEI5", autoAck: true, consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        static async Task publish(string message, IChannel channel)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync(string.Empty, "SEI5", body);
+        }
+    }
+}
---
+++ SEI5/Publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Publisher
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: "SEI5", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            Console.WriteLine("Publisher started");
+            while (true)
+            {
+                Console.WriteLine("Type message to be sent to consumer: ");
+                var input = Console.ReadLine();
+
+                Console.WriteLine("Publishing message to consumer");
+                await publish(input ?? "", channel);
+
+                Console.WriteLine("Message sent successfully!");
+            }
+        }
+
+        static async Task publish(string message, IChannel channel)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+
+            await channel.BasicPublishAsync(string.Empty, "SEI5", body);
+        }
+    }
+}
